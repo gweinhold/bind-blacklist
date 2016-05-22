fafe39276e5ab934c95d4fe9ed61f5a51031112b
Geoff Weinhold <gweinhold@gmail.com>
2016-05-21 21:38:14 -0700

    Trimmed to just mvps.org list and fixed bug with commented out hostname being used.

--- update_blackhole.pl
+++ update_blackhole.pl
@@ -2,13 +2,7 @@
 
 use LWP::Simple;
 
-our @URLs = ( 'http://adaway.sufficientlysecure.org/hosts.txt',
-	      'http://winhelp2002.mvps.org/hosts.txt',
-	      'http://hosts-file.net/ad_servers.asp',
-	      'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext',
-	      'http://www.malwaredomainlist.com/hostslist/hosts.txt',
-	      'http://adblock.gjtech.net/?format=unix-hosts' );
-
+our @URLs = ( 'http://winhelp2002.mvps.org/hosts.txt');
 our %done;
 our %domains;
 
@@ -21,7 +15,7 @@ foreach my $url ( @URLs )
 
     foreach my $line ( split /^/, $content )
     {
-	$domains{ lc $1 }++ if $line =~ /127.0.0.1\s+(\S+)/;
+	$domains{ lc $1 }++ if $line =~ /^0.0.0.0\s+(\S+)/;
 	print STDERR '.';
     }
 
@@ -34,3 +28,4 @@ foreach my $domain ( sort keys %domains )
 {
     print 'zone "',$domain,'" { type master; notify no; file "/etc/bind/db.blackhole"; };' . "\n";
 }
+
